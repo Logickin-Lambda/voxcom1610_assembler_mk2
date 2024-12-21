34f3e25371ee3bb87dc0013a2efc4c1dd4a26168
Logickin-Lambda <logickinlambda@protonmail.com>
2024-12-21 22:59:17 +1100

    - Recovered the VOXCOM ISA I actually have lost the ISA of the computer during migration, but fortunately, my VOXCOM video have a footage showing my original final version of my ISA file, so I can reconstruct the machine code without, reverse engineering my original java implementation.

--- src/main.zig
+++ src/main.zig
@@ -3,18 +3,7 @@ const nfd = @import("nfd");
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    std.debug.print("<!-- Skri-A Kaark -->", .{});
 
     const path_opt = try nfd.openFileDialog("txt", null);
     if (path_opt) |path| {
---
+++ src/process/VoxcomLkup.zig
@@ -0,0 +1,85 @@
+/// Here are all of the available opcodes in VOXCOM 1610:
+/// It use 5 bits as opcode, and the remaining 11 bits for operands
+///
+/// -- ALU Operations
+/// 00 000 000 00000000  NOP / NOTEY
+/// 00 001 xxx xxxxxxxx  NAND Register A and B from the ALU
+/// 00 010 xWx xxxxxxxx  Left shift, W indicates whether the operation requires a wrap
+/// 00 011 xWx xxxxxxxx  Right shift, W indicates whether the operation requires a wrap
+/// 00 100 Cxx xxxxxxxx  ADD Register A and B, result to Register S. C indicates if carry is required
+/// 00 101 CBB BBBBBBBB  ADD Register A with a constant, result to Register S. C indicates if carry is required
+/// 00 110 xxx xxxxxxxx  Perform 2's complement with Register A, result to Register S
+/// 00 111 xxx xxxxxxxx  Swap High and Low byte of Register A, result to Register S
+///
+/// -- Memory Operations
+/// 01 000 rrr rrrxxxxx  Pass two register data from the register bank into register A and B respectively
+/// 01 001 rrr xxxxxxxx  Send the value from the result register to one of the registers in the register bank R[0-7]
+/// 01 010 rrr dddddddd  Clear the specified register R[0-7], and assign a value into the low byte based on dddddddd
+/// 01 011 rrr dddddddd  assign a value into the high byte based on dddddddd
+///
+/// RAMPAGE Mode: dividing the 64k word RAM into 256 pages, and the value of @@@@@@@@ is the index offset of the page
+/// 01 100 rrr @@@@@@@@  RAMPAGE write mode: R[rrr] -> page[addr[@@@@@@@@]]
+/// 01 100 rrr @@@@@@@@  RAMPAGE read mode:  page[addr[@@@@@@@@]] -> R[rrr]
+///
+/// Register Indirect Mode: accessing ram address based on the value stored on the register bank
+/// 01 110 rrr RRRxxxxx  Indirect register mode: R[rrr] -> RAM[val_of(RRR)]
+/// 01 111 rrr RRRxxxxx  Indirect register mode: RAM[val_of(RRR)] -> R[rrr]
+///
+/// Stack Mode: Currently not in uses because it is impossible to build a proper push and pop operation within a single CPU cycle
+/// 10 000 rrr xxxxxxxx  Reserved
+/// 10 001 rrr xxxxxxxx  Reserved
+///
+/// Misc Memory Operations
+/// 10 010 xxx kkkkkkkk  Set the RAMPAGE with kkkkkkkk
+/// 10 011 rrr xxxxxxxx  Current Rom Address (aka the current program counter value) into R[rrr]
+///
+/// -- I/O Ports Operations
+/// 10 100 rrr pppppppp  Write Register values R[rrr] to I/O ports IO[pppppppp]
+/// 10 101 rrr pppppppp  Read I/O ports IO[pppppppp] and write the value into a specified register R[rrr]
+/// 10 110 xxx xxxxxxxx  Reserved
+/// 10 111 xxx xxxxxxxx  Reserved
+///
+/// -- Jump and Misc
+/// 11 000 sll llllllll  Direct jump mode: Unconditionally jump by l lines, s for directions
+/// 11 001 rrr xxxxxxxx  Registers Jump mode: Jump to a given address stored in the specified register R[rrr]
+/// 11 010 coe slllllll  Flag Jump mode: when any of the c (carry), o (overflow), or e (even) flags are enabled, it jumps by l lines if the condition met.
+/// 11 011 leg slllllll  Comparator Jump mode: when any of the l (lesser), e (equal), or g (greater) flags are enabled, it jumps by l lines if the condition met.
+/// 11 100 xxx xxxxxxxx  Reserved
+/// 11 101 xxx xxxxxxxx  Reserved
+/// 11 110 xxx xxxxxxxx  Reserved
+///
+/// -- End
+/// 11 111 xxx xxxxxxxx  Halt the program
+pub const Opcodes = enum(u8) {
+    NOP = 0b00_000_000,
+    NAND = 0b00_001_000,
+    LS = 0b00_010_000,
+    LSW = 0b00_010_010,
+    RS = 0b00_011_000,
+    RSW = 0b00_011_010,
+    ADD = 0b00_100_000,
+    ADDC = 0b00_100_100,
+    TCOM = 0b00_110_000,
+    BSWP = 0b00_111_000,
+    MOV = 0b01_000_000, // Includes all addressing mode
+    STPG = 0b10_010_000,
+    ROMA = 0b10_011_000,
+    PORT = 0b10_100_000,
+    JMP = 0b11_000_000,
+    JMPR = 0b11_001_000,
+    JCR = 0b11_010_100,
+    JOV = 0b11_010_010,
+    JEVN = 0b11_010_001,
+    JLZ = 0b11_011_100,
+    JLEZ = 0b11_011_110,
+    JEZ = 0b11_011_010,
+    JNZ = 0b11_011_101,
+    JGZ = 0b11_011_001,
+    JGEZ = 0b11_011_011,
+    END = 0b11_111_000,
+};
+
+pub const OpcodesEasterEgg = enum(u8) {
+    NOTEY = 0b00_000_000,
+    RAMPAGE = 0b10_010_000,
+};
